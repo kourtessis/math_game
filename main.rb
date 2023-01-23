adfb1be222504221b08d2f0ea2d15c4973f6d474
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-23 14:09:26 -0800

    complete main rb. file with updates as getting chomp error

--- main.rb
+++ main.rb
@@ -1,31 +1,31 @@
 require "./player"
 require "./question"
 
+
 player1 = Player.new
 player2 = Player.new
 
 player = 1
 count = 0
 
-# start game with P1 and P2 at 3 lives, P1 goes first
-
+# Start off the game with Both players being alive and 1st player turn
 until player1.lives == 0 && player2.lives == 0
   if player == 1
-    if count != 0
-      puts '***** NEW TURN *****'
+    if count !=0 
+      puts '** NEW TURN **'
     end
     question = Question.new
-    puts "player 1: #{question.question}"
-    print ">>"
+    puts "P1: #{question.question}"
+    print "Enter Answer>"
     answer = gets.chomp.to_i
-    if answer == question.answer
-      puts 'P1: Well done!'
+  if answer == question.answer
+    puts 'P1: Well done!'
       puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
       player = 2
       count +=1
     else
       player1.lives -=1
-      puts 'P1: Try Again'
+      puts 'P1: Whomp whomp, try again!'
       puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
       if player1.lives == 0
         break
@@ -36,6 +36,39 @@ until player1.lives == 0 && player2.lives == 0
 
   end
 
+  if player == 2
+    if count != 0
+      puts '** NEW TURN **'
+    end
+question = Question.new
+    puts "P2: #{question.question}"
+    print "Enter Answer>"
+    answer = gets.chomp.to_i
+    if answer == question.answer
+      puts 'P2: Well done!'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      player = 1
+      count +=1
+    else
+      player2.lives -=1
+      puts 'P2: Whomp whomp, try again'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      if player2.lives == 0
+        break
+      end
+      player = 1
+      count +=1
+    end
+  end
+end
 
 
-end 
+if (player1.lives == 0)
+  puts "Player 2 wins! #{player2.lives}/3"
+  puts '** GAME OVER **'
+end
+if(player2.lives == 0)
+  puts "Player 1 wins! #{player1.lives}/3"
+  puts '** GAME OVER **'
+
+end
