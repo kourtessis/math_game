04428bee237c8abd87d3e453496b101dc7b1dd96
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-23 12:12:48 -0800

    correct/update question.rb file

--- question.rb
+++ question.rb
@@ -2,11 +2,11 @@ class Question
 attr_accessor :question, :answer
 
   def initialize(number1, number2)
-    @number1 = rand(1...20)
-    @number2 = rand(1...20)
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
 
     @question = "What does #{@number1} plus #{@number2} equal?"
-    @answer = number1 + number2
+    @answer = @number1 + @number2
   end 
 
 end
